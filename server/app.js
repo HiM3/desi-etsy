9acfda23b84684fe9745161093a6fea9cf870a67
HiM3 <krmehta1452@gmail.com>
2025-07-12 18:16:17 +0530

    Error solved of the Routing

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["https://desi-etsy.vercel.app", "https://desi-etsy-him3s-projects.vercel.app"],
     credentials: true,
   })
 );
